618e181a9e2f6aa1e7b1a9015f1878bb36879436
CavadJava <cavadjava@gmail.com>
2024-02-08 11:12:15 +0400

    Updated task1, task2 functions

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // Task 1: Change the color of the greeting text
 document.getElementById('colorButton').addEventListener('click', function() {
-    // TODO: Complete the function to change the 'greeting' element's color to a random color
+    document.getElementById('colorButton').style.color = getRandomColor()
+    document.getElementById('colorButton').style.backgroundColor = getRandomColor()
 });
 
 // Task 2: Change the text content of the greeting
 document.getElementById('textButton').addEventListener('click', function() {
-    // TODO: Complete the function to change the text of the 'greeting' element to "Hello, JavaScript!"
+    document.getElementById('textButton').innerText = "Hello, JavaScript!"
+    
 });
 
 // Helper function for generating a random color (Complete for the applicant)
@@ -13,7 +15,9 @@ function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
     for (var i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
+        var randomDecimal  = Math.random() * 16;
+        var randomInteger = Math.floor(randomDecimal)
+        color += letters[randomInteger];
     }
     return color;
 }
